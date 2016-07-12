ec05a7943c41791ed38471bb872fd1470885486d
Yonatan Wolowelsky <grmmph@gmail.com>
2016-07-12 11:09:10 +0200

    add support for non-latin languages

--- slug.js
+++ slug.js
@@ -58,7 +58,6 @@ function slug(string, opts) {
             }
         }
         // char = char.replace(/[^\w\s\-\.\_~]/g, ''); // allowed
-
 				// allow Hebrew, Arabic, Chinese, Japanese, Georgian, Greek and Cyrilic
         char = char.replace(/[^\u0590-\u05FF\u10A0-\u10FF\u0370-\u03FF\u0600-\u06FF\u0400-\u04FF\u4E00-\u9FCC\u3000-\u303F|\u3040-\u309F|\u30A0-\u30FF|\uFF00-\uFFEF|\u4E00-\u9FAF|\u2605-\u2606|\u2190-\u2195|\u203B\w\s\d]/g, ''); // allowed
 
